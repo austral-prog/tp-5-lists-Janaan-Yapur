83274d3f092c24babd0cf9b3c7ff4a7de94314d2
Janaan-Yapur <jyapur@mail.austral.edu.ar>
2026-04-07 11:33:34 -0300

    Update exercise_find_max.py

--- exercise_find_max.py
+++ exercise_find_max.py
@@ -11,7 +11,7 @@ def find_max(lista):
     Returns:
         El valor máximo de la lista o None si está vacía
     """
-      if lista:
+    if lista:
        return max(lista)
     elif not lista:
        return None
