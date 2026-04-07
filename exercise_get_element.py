9d1f5191db474d3d6b1899167ef71a812ac73937
Janaan-Yapur <jyapur@mail.austral.edu.ar>
2026-04-07 11:34:16 -0300

    Update exercise_get_element.py

--- exercise_get_element.py
+++ exercise_get_element.py
@@ -12,7 +12,7 @@ def get_element(lista, indice):
     Returns:
         El elemento en la posición indicada o None si está fuera de rango
     """
-     if indice < len(lista) and indice >= -len(lista):
+    if indice < len(lista) and indice >= -len(lista):
         return lista[indice]
     else:
         return None
